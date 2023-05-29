65bc8b10eace0372a9652f394dec7d159159b48b
Steve35y <stevederiu@yahoo.it>
2023-05-29 22:32:21 +0200

    Adding Tray class and After Image Struct

--- main.cpp
+++ main.cpp
@@ -3,21 +3,54 @@
 
 // Class Design
 
-class Detection {
+// After Images Struct 
+struct AfterImage {
+	cv::Mat image;
+	int difficultyLevel;
 
-	// add image preprocessing and detection algorithms
+	AfterImage(const cv::Mat& img, int level) : image(img), difficultyLevel(level) {}
 };
 
-class Images {
+// Tray class
+class Tray {
+private:
+	cv::Mat beforeImage;
+	std::vector<cv::Mat> afterImages;
 
-	// add before Image
+public:
 	
-	// add leftovers images as a vector maybe (?)
-	// difficulty vars
+// Tray constructor
+	Tray(const cv::Mat& before, const std::vector<cv::Mat>& after, const std::vector<int>& difficultyLevels)
+		: beforeImage(before) {
+		for (size_t i = 0; i < after.size(); ++i) {
+			afterImages.push_back(AfterImage(after[i], difficultyLevels[i]));
+		}
+	}
+
+	cv::Mat getBeforeImage() const {
+		return beforeImage;
+	}
+
+	std::vector<AfterImage> getAfterImages() const {
+		return afterImages;
+	}
+
+	void setBeforeImage(const cv::Mat& image) {
+		beforeImage = image;
+	}
+
+	void setAfterImages(const std::vector<cv::Mat>& images, const std::vector<int>& difficultyLevels) {
+		afterImages.clear();
+		for (size_t i = 0; i < images.size(); ++i) {
+			afterImages.push_back(AfterImage(images[i], difficultyLevels[i]));
+		}
+	}
+
+	
 };
 
 
 int main(int argc, char** argv)
 {
-
+	return 0;
 }
