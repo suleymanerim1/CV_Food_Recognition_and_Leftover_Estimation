9123f69d247d6d524a132002006b45d5ff246616
Steve35y <stevederiu@yahoo.it>
2023-05-29 22:09:24 +0200

    Main

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+// Class Design
+
+class Detection {
+
+	// add image preprocessing and detection algorithms
+};
+
+class Images {
+
+	// add before Image
+	
+	// add leftovers images as a vector maybe (?)
+	// difficulty vars
+};
+
+
+int main(int argc, char** argv)
+{
+
+}
