313082aaa8484079f2a1c19fe79811904aa86414
Stefano Deriu <stevederiu@yahoo.it>
2023-06-01 21:14:33 +0200

    Evaluation.cpp and .h updated with averageprecision function, to perform mAP it is easy to perform a standard mean across all the averageprecisions performed for each ID, as I said in whatsapp theres a problem with the average precision calculation, which following the theory is correct but it doesnt make sense on sets of 1 Rect.

--- evaluation.cpp
+++ evaluation.cpp
@@ -43,6 +43,104 @@ float get_iou(const cv::Rect& groundTruth, const cv::Rect& prediction)
     return iou;
 }
 
+float averagePrecision(std::vector<cv::Rect>& groundTruths, std::vector<cv::Rect>& predictions)
+{
+    // 1) Sort Predictions, as confidence score I use IoU, I am assuming that the vectors have predictions of the initial images at the same positions
+    std::vector<float> score(predictions.size());
+    for (size_t i = 0; i < predictions.size(); ++i) {
+        score[i] = get_iou(groundTruths[i], predictions[i]);
+    }
+
+    // Sort predictions vector based on confidence scores
+    for (int i = 0; i < predictions.size() - 1; ++i)
+    {
+        for (int j = 0; j < predictions.size() - i - 1; ++j)
+        {
+            if (score[j] < score[j + 1])
+            {
+                cv::Rect temp = predictions[j];
+                predictions[j] = predictions[j + 1];
+                predictions[j + 1] = temp;
+                cv::Rect temp2 = groundTruths[j];
+                groundTruths[j] = groundTruths[j + 1];
+                groundTruths[j + 1] = temp2;
+                float temp3 = score[j];
+                score[j] = score[j + 1];
+                score[j + 1] = temp3;
+            }
+        }
+    }
+
+    for (const auto& rect : predictions) {
+        std::cout << "Predictions: " << rect.x
+            << ", " << rect.y
+            << ", " << rect.width
+            << ", " << rect.height << std::endl;
+    }
+    for (const auto& rect : groundTruths) {
+        std::cout << "groundTruths: " << rect.x
+            << ", " << rect.y
+            << ", " << rect.width
+            << ", " << rect.height << std::endl;
+    }
+
+    int truePositives = 0;
+    int falsePositives = 0;
+    int falseNegatives = 0;
+    std::vector<float> precision(predictions.size());
+    std::vector<float> recall(predictions.size());
+
+    float ap = 0;
+    float prevRecall = 0.0;
+    float maxPrecision = 0.0;
+    std::cout << "Predictions.size: " << predictions.size() << std::endl << std::endl;
+
+    // PASCAL VOC 11 Point Interpolation Method for calculating the Average Precision (AP)
+    for (int i = 0; i < predictions.size(); ++i)
+    {
+        std::cout << "i: " << i << std::endl;
+        if (score[i] >= 0.5) {  // IoU threshold = 0.5
+            truePositives++;
+        }
+        else {
+            falsePositives++;
+        }
+        std::cout << "truepos: " << truePositives << std::endl;
+        std::cout << "falsepos: " << falsePositives << std::endl;
+        falseNegatives = groundTruths.size() - truePositives;
+        std::cout << "falseneg: " << falseNegatives << std::endl;
+
+        precision[i] = static_cast<float>(truePositives) / (truePositives + falsePositives);
+        recall[i] = static_cast<float>(truePositives) / (truePositives + falseNegatives);
+        std::cout << "Precision[i]: " << precision[i] << std::endl;
+        std::cout << "Recall[i]: " << recall[i] << std::endl;
+
+
+
+
+        // Calculate the maximum precision at the current recall level
+        if (precision[i] > maxPrecision)
+            maxPrecision = precision[i];
+        std::cout << "Max Precision: " << maxPrecision << std::endl;
+
+        // Calculate the average precision using the PASCAL VOC 11 Point Interpolation Method
+        if (recall[i] != prevRecall)
+        {
+            ap += maxPrecision;
+            prevRecall = recall[i];
+        }
+    }
+
+    // Calculate the final average precision
+    ap /= 11.0;
+
+    std::cout << "Average Precision (AP): Maxprecision/11 " << ap << std::endl << std::endl << std::endl;
+
+    return ap;
+}
+
+
+
 float mIoU(const std::vector<cv::Rect>& groundTruths, const std::vector<cv::Rect>& predictions) {
     if (groundTruths.size() != predictions.size()) {
         std::cerr << "Error: Number of ground truth objects is not equal to the number of predicted objects." << std::endl;
--- evaluation.h
+++ evaluation.h
@@ -6,6 +6,9 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/core.hpp>
 
+
+
+
 /* IoU
 
 What we need for the evaluation functions:
@@ -17,3 +20,5 @@ The value ranges from 0 to 1.
 
 float get_iou(const cv::Rect& groundTruth, const cv::Rect& prediction);
 float mIoU(const std::vector<cv::Rect>& groundTruths, const std::vector<cv::Rect>& predictions);
+
+float averagePrecision(std::vector<cv::Rect>& groundTruths, std::vector<cv::Rect>& predictions);
